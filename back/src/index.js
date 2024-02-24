76e4e877d8d7e59c116a4c0af1a1c6e61cb65994
Arturo Vaine <arturo.vaine@gmail.com>
2024-02-24 18:11:12 -0300

    fix(api): fix compatibility of packages (cors,jwt,swagger), and login endpoint security issues

--- back/src/index.js
+++ back/src/index.js
@@ -5,17 +5,16 @@ import * as fastifyMySQL from '@fastify/mysql';
 
 import bcrypt from 'bcrypt';
 import jwt from 'fastify-jwt';
-//import * as jwt from '@fastify/jwt@5.0.0';
 import authPlugin from './authPlugin.js';
-
+import cors from '@fastify/cors'
 import dotenv from 'dotenv';
 dotenv.config({ path: '../.env' });
 
-import swagger from '@fastify/swagger';
-//import fastifySwagger from 'fastify-swagger';
 import routes from './routes/index.js';
 
 const app = fastify({ logger: true });
+
+app.register(cors, { origin: true });
 
 await app.register(import('@fastify/swagger'))
 await app.register(import('@fastify/swagger-ui'))
@@ -30,26 +29,27 @@ app.post('/login', async (request, reply) => {
 
   try {
     const connection = await app.mysql.getConnection();
-    const [rows] = await connection.query(`
-      SELECT * FROM cliente
-      WHERE username = '${username}';`);
+    const [rows] = await connection.query('SELECT * FROM cliente WHERE username = ?', [username]);
     connection.release();
 
-    let encryptedPassword = rows[0].senha;
-    const match = await bcrypt.compare(password,encryptedPassword);
-    
-    if (match) {
-      const { cliente_id, email, username, cpf } = rows[0];
-      const token = app.jwt.sign({ cliente_id, email, username, cpf }, { expiresIn: '24h' });
-      reply.send({ token });
+    if (rows.length > 0) {
+      let encryptedPassword = rows[0].senha;
+      const match = await bcrypt.compare(password, encryptedPassword);
+      
+      if (match) {
+        const { cliente_id, email, username, cpf } = rows[0];
+        const token = app.jwt.sign({ cliente_id, email, username, cpf }, process.env.JWT_SECRET, { expiresIn: '24h' });
+        reply.send({ token });
+      } else {
+        reply.status(401).send({ message: 'Usuário ou senha incorretos' });
+      }
     } else {
       reply.status(401).send({ message: 'Usuário ou senha incorretos' });
     }
-
   } catch (error) {
-    reply.code(400).send(error);
+    reply.code(500).send({ message: "Ocorreu um erro no login." });
+    console.error(error);
   }
-  
 });
 
 app.decorate("authenticate", async (request, reply) => {
